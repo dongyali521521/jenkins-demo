403c7373c345015c06644e6dcb4d582c1bc18ca8
Your Name <you@example.com>
2019-09-08 21:30:54 +0800

    6666

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, Kubernetes！I'm from Jenkins CI！55555")
+	fmt.Println("Hello, Kubernetes！I'm from Jenkins CI！66666")
 	fmt.Println("BRANCH_NAME:", os.Getenv("branch"))
 }
